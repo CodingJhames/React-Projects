8aa00dee8d9a301ab6ecdffae0e73cce6dc6b45b
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-10-08 19:41:28 -0500

    styles added

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,8 +1,25 @@
+import { Paciente } from "./Paciente"
 
 
 const ListadoPacientes = () => {
   return (
-    <h2>ListadoPacientes</h2>
+
+    <div className="sm:w-1/2 lg:w-3/5 sm:h-screen overflow-y-scroll">
+        <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+        <p className="text-xl mt-5 mb-10 text-center">
+            Administra los {''}
+            <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
+        </p>
+
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+
+    </div>
+
+    
   )
 }
 
