9b5a78658b236b4154d0effe69992b4972465ff4
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-05-22 20:30:02 -0500

    added caelum folder

---
+++ caelum/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { LayoutBoard } from './components/LayoutBoard'
+
+function App() {
+  
+
+  return (
+    <>
+      <LayoutBoard />
+    </>
+  )
+}
+
+export default App
---
+++ caelum/src/components/LayoutBoard.tsx
@@ -0,0 +1,15 @@
+import { FetchData } from '../helpers/FetchData';
+
+
+
+
+export const LayoutBoard = () => {
+  return (
+    <>
+        <h1>Board Cards</h1>
+        
+    
+    </>
+  )
+}
+
