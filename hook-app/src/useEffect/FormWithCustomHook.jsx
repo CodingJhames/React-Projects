2a5eca6b1dd7318605a10b7dcc384d29e5d7a8ae
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-05-30 19:35:16 -0500

    onReset added

--- hook-app/src/useEffect/FormWithCustomHook.jsx
+++ hook-app/src/useEffect/FormWithCustomHook.jsx
@@ -1,19 +1,18 @@
 import { useEffect, useState } from "react"
 import { useForm } from "../hooks/useForm";
 
-
-
 export const FormWithCustomHook= () => {
 
-    const { formState, onInputChange, username, email, password  } = useForm({
+    const { formState, onInputChange, username, email, password, onResetForm  } = useForm({
         username:'',
         email:"",
         password: "",
     });
 
-    // const {username, email, password  } = formState;
     
 
+    // const {username, email, password  } = formState;
+    
   return (
 
     <>
@@ -46,6 +45,8 @@ export const FormWithCustomHook= () => {
             value={ password }
             onChange={ onInputChange }
         />
+
+        <button onClick={ onResetForm } className="btn btn-primary mt-2">Reset</button>
 
     </>
 
